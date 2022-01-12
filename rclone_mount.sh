05eb6f71e5f041de9487d2b4f817b0eb0673c1b2
hpdiddy <76453609+HPDiddy@users.noreply.github.com>
2022-01-12 08:40:53 +0000

    Update rclone_mount.sh

--- rclone_mount.sh
+++ rclone_mount.sh
@@ -8,12 +8,8 @@ if [ -f $USER/mnt/gdrive ]
 	then
 rclone mount gcrypt: ~/mnt/gdrive --allow-other --allow-non-empty --cache-db-purge --buffer-size 32M --use-mmap --dir-cache-time 72h --drive-chunk-size 16M  --timeout 1h  --vfs-cache-mode writes --vfs-read-chunk-size 128M --vfs-read-chunk-size-limit 1G &
 else
-echo "The directory $USER/mnt/gdrive does not exist, Do you want to create it?[Y/n]"
-read answer
-if [ $answer = "Y" ]
-	then
-	mkdir $USER/mnt/gdrive
-	fi
+echo "The directory $USER/mnt/gdrive does not exist, Creating it now"
+sudo mkdir /$USER/home/mnt/gdrive
 sleep 2
 echo "Drive mounted at $USER/mnt/gdrive"
 else
